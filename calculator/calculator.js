28ee2c5ffb5330c420cf07b3a7399ff51127b7f1
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-05-18 23:28:21 +0800

    fix the f**k bug about the js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -7,7 +7,7 @@ class Character {
 
 class Atk {
     constructor(base_ATK, favorability_ATK, back_row_ATK, equip_ATK, passive_skill_ATK) {
-        this.back_row_ATK = base_ATK;
+        this.base_ATK = base_ATK;
         this.favorability_ATK = favorability_ATK;
         this.back_row_ATK = back_row_ATK;
         this.equip_ATK = equip_ATK;
@@ -15,9 +15,9 @@ class Atk {
     }
 
     getATK() {
-        
-        var total_ATK = 1;
-        total_ATK = (base_ATK + favorability_ATK + back_row_ATK) * (1 + 0.01 * equip_ATK + 0.01 * passive_skill_ATK);
+
+        var total_ATK = (this.base_ATK + this.favorability_ATK + this.back_row_ATK) * (1 + 0.01 * this.equip_ATK + 0.01 * this.passive_skill_ATK);
+        console.log(total_ATK);
 
         return total_ATK;
 
@@ -27,15 +27,16 @@ class Atk {
 
 function display(){
 
-    base_ATK = base_ATK.value;
-    favorability_ATK = favorability_ATK.value,
-    back_row_ATK = back_row_ATK.value,
-    equip_ATK = equip_ATK.value,
-    passive_skill_ATK = passive_skill_ATK.value;
+    console.log(base_ATK.value, favorability_ATK.value, back_row_ATK.value, equip_ATK.value, passive_skill_ATK.value);
+    var bATK = Number(base_ATK.value);
+    var fATK = Number(favorability_ATK.value);
+    var brATK = Number(back_row_ATK.value);
+    var eATK = Number(equip_ATK.value);
+    var psATK = Number(passive_skill_ATK.value);
 
-    console.log(base_ATK, favorability_ATK, back_row_ATK, equip_ATK, passive_skill_ATK);
-    var ATK = new Atk(back_row_ATK, favorability_ATK, back_row_ATK, equip_ATK, passive_skill_ATK);
-    total_ATK = ATK.getATK();
+    console.log(bATK, fATK, brATK, eATK, psATK);
+    var ATK = new Atk(bATK, fATK, brATK, eATK, psATK);
+    var total_ATK = ATK.getATK();
     console.log(total_ATK);
 
     let el = document.getElementById('output');
