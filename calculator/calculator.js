84172d2c98ba2326d0fe4182ba1b4d500f8d212e
Chunill <yces980096@gmail.cpm>
2022-05-18 23:33:46 +0800

    yes

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,7 @@
 
 
 function display(){
+    
     base_ATK = Number(base_ATK.value);
     favorability_ATK = Number(favorability_ATK.value),
     back_row_ATK = Number(back_row_ATK.value),
@@ -12,5 +13,7 @@ function display(){
     total_ATK = (base_ATK + favorability_ATK) * (1 + 0.01 * equip_ATK + 0.01 * passive_skill_ATK);
     console.log(total_ATK);
     console.log(base_ATK, favorability_ATK, equip_ATK, passive_skill_ATK);
+    
+
     el.textContent = total_ATK;
 }
