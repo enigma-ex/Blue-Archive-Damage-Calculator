77beb063cb165f4e77d2280337a165087841ee74
GoldOrange261 <63157008+GoldOrange261@users.noreply.github.com>
2022-05-18 23:34:34 +0800

    change the ATKs variable to const

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -28,11 +28,11 @@ class Atk {
 function display(){
 
     console.log(base_ATK.value, favorability_ATK.value, back_row_ATK.value, equip_ATK.value, passive_skill_ATK.value);
-    var bATK = Number(base_ATK.value);
-    var fATK = Number(favorability_ATK.value);
-    var brATK = Number(back_row_ATK.value);
-    var eATK = Number(equip_ATK.value);
-    var psATK = Number(passive_skill_ATK.value);
+    const bATK = Number(base_ATK.value);
+    const fATK = Number(favorability_ATK.value);
+    const brATK = Number(back_row_ATK.value);
+    const eATK = Number(equip_ATK.value);
+    const psATK = Number(passive_skill_ATK.value);
 
     console.log(bATK, fATK, brATK, eATK, psATK);
     var ATK = new Atk(bATK, fATK, brATK, eATK, psATK);
