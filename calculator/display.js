24e15eb9b4613b5d3ac0515b26f52b20d85e8a2d
Chunill <yces980096@gmail.cpm>
2022-05-29 05:48:29 +0800

    test: this is test commit test:

--- calculator/display.js
+++ calculator/display.js
@@ -1,5 +1,6 @@
 function get_json(){
     let jsonData;
+    console.log(1);
     fetch('output.json')
     .then(function(response) {
         return response.json();
@@ -7,7 +8,9 @@ function get_json(){
     .then(function(myJson) {
         jsonData = myJson;
     });
+    console.log(jsonData);
     return jsonData;
 }
-
+get_json();
 console.log(get_json());
+// test
