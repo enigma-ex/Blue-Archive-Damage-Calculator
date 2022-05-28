7423ac52b89816bcbfd134404b50a1623e6d4877
Chunill <yces980096@gmail.cpm>
2022-05-29 06:26:42 +0800

    style: i variable's name becomes index

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -74,7 +74,7 @@ function display(){
     popup();
 }
 
-function add_input_check(i){
+function add_input_check(index){
     let add_item = document.createElement("div");
     add_item.className = "add_item";
 
@@ -91,14 +91,14 @@ function add_input_check(i){
     delete_button.className = "button_type2";
     add_item.append(delete_button);
 
-    let add_input = document.getElementById("add_input"+i);
+    let add_input = document.getElementById("add_input"+index);
     add_input.appendChild(add_item);
     delete_button.addEventListener('click',function(){
         this.parentNode.remove();
     })
 
     input_check.addEventListener('keydown',function(event){
-        let enter_to_add_input = document.getElementById('add_input'+i).getElementsByTagName('input');
+        let enter_to_add_input = document.getElementById('add_input'+index).getElementsByTagName('input');
         let arr = Array.prototype.slice.call(enter_to_add_input);
         if (event.key === "Enter" && arr.indexOf(event.target) < arr.length - 1){
             arr[arr.indexOf(event.target) + 1].focus();
