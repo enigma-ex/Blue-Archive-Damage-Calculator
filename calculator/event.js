e51f21649024fe3192d70d82e92af33d4f90c049
chunill <yces980096@gmail.com>
2022-05-29 18:15:57 +0800

    fix: fix calculate buff value display.js -> change the id event.js -> index parameter name change to value_or_percentage calculator.js -> add buff_value decide

--- calculator/event.js
+++ calculator/event.js
@@ -7,31 +7,35 @@ function handle_enter(event){
     }
 }
 
-function add_input_check(index){
+function add_input_check(value_or_percentage){
     let add_item = document.createElement("div");
     add_item.className = "add_item";
-
     add_item.innerHTML += "<br>";
-
     let input_check = document.createElement("input");
     input_check.text = "text";
-    input_check.className = "buff_percentage_ATK";
-    input_check.placeholder = "增益/減益效果(預設+0%)";
-    add_item.appendChild(input_check);
 
+    if (value_or_percentage === "value") {
+        input_check.className = "buff_value_ATK";
+        input_check.placeholder = "增益/減益效果(預設+0)";
+    }
+    else if (value_or_percentage === "percentage") {
+        input_check.className = "buff_percentage_ATK";
+        input_check.placeholder = "增益/減益效果(預設+0%)";
+   }
+    add_item.appendChild(input_check);
     let delete_button = document.createElement("button");
     delete_button.textContent = "刪除";
     delete_button.className = "button_type2";
     add_item.append(delete_button);
 
-    let add_input = document.getElementById("add_input"+index);
+    let add_input = document.getElementById("add_input_"+value_or_percentage);
     add_input.appendChild(add_item);
     delete_button.addEventListener('click',function(){
         this.parentNode.remove();
     })
 
     input_check.addEventListener('keydown',function(event){
-        let enter_to_add_input = document.getElementById('add_input'+index).getElementsByTagName('input');
+        let enter_to_add_input = document.getElementById('add_input_'+value_or_percentage).getElementsByTagName('input');
         let arr = Array.prototype.slice.call(enter_to_add_input);
         if (event.key === "Enter" && arr.indexOf(event.target) < arr.length - 1){
             arr[arr.indexOf(event.target) + 1].focus();
